b09ade74b3e961b56efc936aad9154fb9aa031c0
Jonathan57 <jonathan.hinsberger@gmail.com>
2014-12-04 13:26:23 +0100

    Version 0 application initiale

---
+++ gsbMVC/vues/v_sommaire.php
@@ -0,0 +1,64 @@
+﻿    <!-- Division pour le sommaire -->
+      <div id="menuList">
+    <h2><table>
+        <tr>
+				<tr id="smenu1">
+				<a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Accueil </a>
+		</tr>
+		</tr>
+		<tr id="smenu1">
+              <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie Thème/Mot </a>
+		</tr>
+        <tr id="smenu2">
+              <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Consulter </a>
+		</tr>
+    </table></h2>
+	
+    </div>
+	<br/>
+	<h1>Inscripition rapide de Thèmes ou Mots:</h1>
+	<br/>
+	<table class='tabNonQuadrille'>
+<tr><br/>
+	<td></td><td></td><td></td><td>Thème :</td>
+	<td>
+		<input  type='text' name=dateFrais  size='30' maxlength='45'>
+	</td>
+	<td><input type='submit' value='Valider' name='valider'></td>
+</tr>
+
+</table>
+<br/>
+<table class='tabNonQuadrille'>
+
+<tr>
+	<td></td><td></td><td></td><td></td><td></td><td><td></td><td></td></td><td></td><td></td><td>Mot: </td>
+	<td>
+		<input  type='text' name=dateFrais  size='30' maxlength='45'>
+	</td>
+	<td>Thème(s): </td>
+	<td>
+		
+
+    <SELECT name="nom[]" size="3" multiple >
+	<OPTION>Bonbon</option>
+    <OPTION>Fleurs</option>
+    <OPTION>Semaine</option>
+    <OPTION>Mois</option>
+    <OPTION>Couleur</option>
+    </SELECT>
+
+	</td>
+	<td><input type='submit' value='Valider' name='valider'></td>
+	
+</tr>
+</table>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+ 
+    
